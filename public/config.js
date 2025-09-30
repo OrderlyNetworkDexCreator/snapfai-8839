ae818c44f9f515d449ffc039f8b6ef6e751157b6
Mario Reder <mario.reder@pm.me>
2025-09-30 05:38:33 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "snapfai",
   "VITE_ORDERLY_BROKER_NAME": "snapfai",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
