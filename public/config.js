af93fc4e37165b89c5fef178ac3d024e636fafd5
Mario Reder <mario.reder@pm.me>
2025-11-25 04:44:52 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "An AI-powered Perp Dex",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Swap",
   "VITE_CUSTOM_MENUS": "Affiliate, https://trade.snapfai.com/rewards/affiliate; SnapFAI Agent, https://snapfai.com/snap; Docs, https://docs.snapfai.com/",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
